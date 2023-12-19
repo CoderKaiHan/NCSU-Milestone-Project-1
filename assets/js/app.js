035a5349243de3dff5f3f028ca6c8a8518aa2521
CoderKaiHan <hankaicoder@gmail.com>
2023-12-19 16:14:07 -0500

    Added two items(arrows)

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,37 +9,52 @@ function createCharacter (src,scaleX,position,top,left,zIndex){
     character.style.left = left;
     character.style.zIndex = zIndex;
     playArea.appendChild(character);
-    return character;
-
+    return character
 }
 
-function createItem (src,scaleX,width,height,position,zIndex){
+
+function createItem (src,scaleX,position,width,height,zIndex,top,left){
     const item = document.createElement('img');
     item.src = src;
     item.style.transform = `scaleX(${scaleX})`;
+    item.style.position = position;
     item.style.width =width;
     item.style.height = height;
-    item.style.position = position;
     item.style.zIndex = zIndex;
+    item.style.top = top;
+    item.style.left = left;
     playArea.appendChild(item);
     return item;
 }
 
-function attachItemToCharacter(character,attachedItem,x,y) {
-    const characterRect = character.getBoundingClientRect();
-    const newX = characterRect.left + characterRect.width + x;
-    const newY = characterRect.top +characterRect.height + y;
-    attachedItem.style.position = 'absolute';
-    attachedItem.style.left = newX +'px'
-    attachedItem.style.top = newY + 'px'
-}
+// function attachItemToCharacter(character,attachedItem,xPercentage,yPercentage) {
+//     const characterRect = character.getBoundingClientRect();
+//     const xOffset = (parseFloat(xPercentage)) * characterRect.width;
+//     const yOffset = (parseFloat(yPercentage)) * characterRect.height;
+//     const newX =characterRect.left + characterRect.width + xOffset;
+//     const newY = characterRect.top + characterRect.height + yOffset;
+
+//     attachedItem.style.position = 'absolute';
+//     attachedItem.style.left = newX +'px'
+//     attachedItem.style.top = newY + 'px'
+// }
+
+// function attachItemToCharacter(attachedItem,character,left,top) {
+//     const characterRect = character.getBoundingClientRect();
+//     const leftOffset = (left / 100) * characterRect.width;
+//     const topOffset = (top / 100) * characterRect.height;
+
+//     attachedItem.style.position = 'absolute';
+//     attachedItem.style.left = characterRect.left + leftOffset + 'px';
+//     attachedItem.style.top = characterRect.top + topOffset + 'px';
+// }
 
 const character1 = createCharacter('/assets/Images/archer light.png',1,'absolute','85%','10%','-1')
 
 const character2 = createCharacter('/assets/Images/archer_elf.png',-1,'absolute','85%','80%','-1')
 
-const bow1 = createItem('/assets/Images/Bow and Arrow Set/Png/Small/Bow3.png',1,'20px','40px','absolute','1')
+const bow1 = createItem('/assets/Images/Bow and Arrow Set/Png/Small/Bow3.png',1,'absolute','20px','40px','1','86%','14%')
 
-const bow2 = createItem('/assets/Images/Bow and Arrow Set/Png/Small/Bow3.png',-1,'20px','40px','absolute','1')
+const bow2 = createItem('/assets/Images/Bow and Arrow Set/Png/Small/Bow3.png',-1,'absolute','20px','40px','1','86%','81%')
 
-attachItemToCharacter(character1,bow1,'-10%','-20%')
+// attachItemToCharacter(bow1, character1,600,145)
