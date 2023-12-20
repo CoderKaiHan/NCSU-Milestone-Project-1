b9c324fb0fce153a3bd0f54cd84b3da9832ab418
CoderKaiHan <hankaicoder@gmail.com>
2023-12-19 21:24:55 -0500

    added two arrows, create a moveItem function and move bows and arrow to the position with the characters & working on an animation function to make the arrow move in curve when click the mouse(unfinished)

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,49 +12,53 @@ function createCharacter (src,scaleX,position,top,left,zIndex){
     return character
 }
 
-
-function createItem (src,scaleX,position,width,height,zIndex,top,left){
+function createItem (src,position,width,height,top,left,zIndex){
     const item = document.createElement('img');
     item.src = src;
-    item.style.transform = `scaleX(${scaleX})`;
     item.style.position = position;
     item.style.width =width;
     item.style.height = height;
-    item.style.zIndex = zIndex;
     item.style.top = top;
     item.style.left = left;
+    item.style.zIndex = zIndex;
     playArea.appendChild(item);
     return item;
 }
 
-// function attachItemToCharacter(character,attachedItem,xPercentage,yPercentage) {
-//     const characterRect = character.getBoundingClientRect();
-//     const xOffset = (parseFloat(xPercentage)) * characterRect.width;
-//     const yOffset = (parseFloat(yPercentage)) * characterRect.height;
-//     const newX =characterRect.left + characterRect.width + xOffset;
-//     const newY = characterRect.top + characterRect.height + yOffset;
-
-//     attachedItem.style.position = 'absolute';
-//     attachedItem.style.left = newX +'px'
-//     attachedItem.style.top = newY + 'px'
-// }
-
-// function attachItemToCharacter(attachedItem,character,left,top) {
-//     const characterRect = character.getBoundingClientRect();
-//     const leftOffset = (left / 100) * characterRect.width;
-//     const topOffset = (top / 100) * characterRect.height;
-
-//     attachedItem.style.position = 'absolute';
-//     attachedItem.style.left = characterRect.left + leftOffset + 'px';
-//     attachedItem.style.top = characterRect.top + topOffset + 'px';
-// }
+function moveItem (item,right,down,scaleX,rotate){
+    item.style.transform = `translate(${right}px, ${down}px) scaleX(${scaleX}) rotate(${rotate}deg)`
+}
 
 const character1 = createCharacter('/assets/Images/archer light.png',1,'absolute','85%','10%','-1')
 
 const character2 = createCharacter('/assets/Images/archer_elf.png',-1,'absolute','85%','80%','-1')
 
-const bow1 = createItem('/assets/Images/Bow and Arrow Set/Png/Small/Bow3.png',1,'absolute','20px','40px','1','86%','14%')
+const bow1 = createItem('/assets/Images/Bow and Arrow Set/Png/Small/Bow3.png','absolute','20px','40px','85%','10%','1')
 
-const bow2 = createItem('/assets/Images/Bow and Arrow Set/Png/Small/Bow3.png',-1,'absolute','20px','40px','1','86%','81%')
+const bow2 = createItem('/assets/Images/Bow and Arrow Set/Png/Small/Bow3.png','absolute','20px','40px','85%','80%','1')
 
-// attachItemToCharacter(bow1, character1,600,145)
+const arrow1 = createItem('/assets/Images/arrow (1).png','absolute','8px','50px','85%','10%')
+
+const arrow2 = createItem('/assets/Images/arrow (1).png','absolute','8px','50px','85%','80%')
+
+moveItem(bow1,45,2,1,0)
+moveItem(bow2,0,0,-1,0)
+moveItem(arrow1,45,-3,1,90)
+moveItem(arrow2,10,-4,-1,90)
+
+
+const canvas = document.getElementById('myCanvas');
+const ctx =canvas.getContext('2d');
+
+let playAni = false;
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+animate();
+
+canvas.addEventListener("click",()=>{
+    playAni = true;
+});
