33cfaf9a20cf0c597421abae76a1c6b4f520d0ef
CoderKaiHan <hankaicoder@gmail.com>
2024-01-02 19:45:59 -0500

    Modified appedTargets function && Defined arrow1DetectsTargets function && Modified players control function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,15 @@
 //define a playArea variable
 const playArea = document.querySelector('#play-area');
-
 const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2')
 
 player1.style.top = '240px';
 player1.style.left = '100px';
+player2.style.top = '240px';
+player2.style.left = '740px';
 
-//player1 control function
+
+//players control function
 document.addEventListener('keydown', function(event) {
     // Get player1 current position
     const player1CurrentPosition = parseInt(player1.style.top) || 0;
@@ -24,7 +27,24 @@ document.addEventListener('keydown', function(event) {
             player1NewPosition= Math.min(player1CurrentPosition + moveAmount, 540);
             break; 
     }
-    player1.style.top = player1NewPosition + 'px'
+    player1.style.top = player1NewPosition + 'px';
+    
+    // Get player2 current position
+    const player2CurrentPosition = parseInt(player2.style.top) || 0;
+
+    // Adjust the position based on the key pressed and restrict it within the play area
+    let player2NewPosition;
+    switch(event.key) {
+        case 'ArrowUp':
+            event.preventDefault();
+            player2NewPosition= Math.max(player2CurrentPosition - moveAmount, 0)
+            break;
+        case 'ArrowDown':
+            event.preventDefault();
+            player2NewPosition= Math.min(player2CurrentPosition + moveAmount, 540);
+            break; 
+    }
+    player2.style.top = player2NewPosition + 'px';
 });
 
 //player1 release arrow function
@@ -32,9 +52,6 @@ document.addEventListener('keydown',function(event){
     //Get player1 current top position
     const player1CurrentTop = parseInt(player1.style.top);
     const player1CurrentLeft = parseInt(player1.style.left);
-    //check player1 location
-    console.log('player1CurrentTop:', player1CurrentTop);
-    console.log('player1CurrentLeft:', player1CurrentLeft);
 
 
     //Get player1 arrow current location
@@ -48,24 +65,21 @@ document.addEventListener('keydown',function(event){
         arrow1.src = '/assets/Images/Bow and Arrow Set/Png/Medium/Arrow5.png';
         arrow1.style.position ='absolute';
         arrow1.style.top = arrow1CurrentTop + 'px';
-        // arrow1.style.left = arrow1CurrentLeft + 'px';
         arrow1.style.width = '60px';
         arrow1.style.height = '10px';
-        //check arrow location
-        console.log('arrow1CurrentTop:', arrow1CurrentTop);
-        console.log('arrow1CurrentLeft:', arrow1CurrentLeft);
 
         function moveArrow(){
-            if(arrow1CurrentLeft < 800){
+            if(arrow1CurrentLeft < 640){
                 arrow1CurrentLeft +=50;
                 arrow1.style.left = arrow1CurrentLeft + 'px';
+                arrow1DectectsTargets(arrow1,arrow1CurrentLeft,arrow1CurrentTop); 
                 setTimeout(moveArrow, getShootSpeed())
+            }else{
+                arrow1.style.display = 'none';
             }
         }
 
-        moveArrow()
-
-        
+        moveArrow()       
     }
 
     if(arrow1){
@@ -111,20 +125,48 @@ startButton.addEventListener('click',function(){
 
 
 //Append Targets 
+const targetsArray = [];
+
 function appendTargets (){
     let targets = document.createElement('img');
-    targets.src = '/assets/Images/Bow and Arrow Set/Png/Medium/Bow1.png';
+    targets.src = '/assets/Images/Targets/xbullet.png';
 
     const maxLeft = 700;
     const minLeft = 200;
     const randomLeft = Math.floor(Math.random()*(maxLeft - minLeft + 1) + minLeft);
     
     targets.style.position = 'absolute';
+    targets.style.width = '30px';
+    targets.style.height = '30px';
     targets.style.left = randomLeft + 'px';
     targets.style.top = Math.floor(Math.random() * (playArea.clientHeight - targets.height)) + 'px';
 
     playArea.appendChild(targets)
+
+    targetsArray.push(targets);
+
+    return targets;
 }
+
+//Arrow 1 detects targets
+function arrow1DectectsTargets (arrow1,arrow1Left,arrow1Top){
+    for (let i = 0; i < targetsArray.length; i++){
+        const targets = targetsArray[i];
+        const targetsLeft = parseInt(targets.style.left);
+        const targetsTop = parseInt(targets.style.top);
+        const targetsWidth = parseInt(targets.style.width);
+        const targetsHeight = parseInt(targets.style.height);
+
+        if (arrow1Left + 60 >= targetsLeft && arrow1Left <= targetsLeft + targetsWidth && arrow1Top >= targetsTop && arrow1Top <= targetsTop + targetsHeight){  
+            targetsArray.splice(i,1);
+            playArea.removeChild(targets);
+            arrow1.style.display = 'none';
+            appendTargets();
+;    }
+  }
+}
+
+
 
 
 
